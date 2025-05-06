bf8e3009dac080f0045e6f541a0a64dace9bca57
artemqua3344 <artem.nek12@yandex.ru>
2025-05-06 13:51:34 +0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,73 @@
+document.getElementById('contact-form').addEventListener('submit', function (event) {
+  event.preventDefault(); // Отменяем отправку формы
+
+  const form = event.target;
+  const formData = new FormData(form);
+
+  fetch('http://127.0.0.1', {
+    method: 'POST',
+    body: formData,
+  })
+    .then(response => {
+      if (!response.ok) throw new Error("Ошибка сети");
+      return response.text(); // или .json() если ожидается JSON
+    })
+    .then(data => {
+      alert('Форма успешно отправлена!');
+      console.log('Ответ сервера:', data);
+      form.reset(); // очищаем форму
+    })
+    .catch(error => {
+      alert('Ошибка при отправке формы');
+      console.error('Ошибка:', error);
+    });
+});
+
+
+
+document.getElementById('contact-form2').addEventListener('submit', function (e) {
+  e.preventDefault();
+
+  const name = document.getElementById('name2').value.trim();
+  const agreement = document.getElementById('agreement2').checked;
+
+  if (!name || !agreement) {
+    Swal.fire({
+      icon: 'error',
+      title: 'Ошибка',
+      text: 'Пожалуйста, заполните обязательные поля.'
+    });
+    return;
+  }
+
+  Swal.fire({
+    icon: 'success',
+    title: 'Данные отправлены',
+    html: `<p><strong>Имя:</strong> ${name}</p>`
+  });
+});
+
+
+
+async function getJsonValues(fileName = 'data.json') {
+  try {
+    const response = await fetch(fileName);
+    if (!response.ok) {
+      throw new Error('Ошибка при загрузке JSON');
+    }
+
+    const jsonArray = await response.json();
+    const values = jsonArray.flatMap(obj => Object.values(obj));
+
+    // Выводим на страницу
+    document.getElementById('output').textContent = JSON.stringify(values, null, 2);
+    return values;
+
+  } catch (error) {
+    console.error('Ошибка:', error);
+    document.getElementById('output').textContent = 'Ошибка при загрузке данных.';
+    return [];
+  }
+}
+
+getJsonValues();
